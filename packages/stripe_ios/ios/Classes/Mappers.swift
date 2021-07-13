c0bcd3db38d22af77b8844398967bbe27147ebf9
Yoshiaki Oshima <oshima@buildman.co.jp>
2021-07-13 11:10:11 +0900

    fix mappers

--- packages/stripe_ios/ios/Classes/Mappers.swift
+++ packages/stripe_ios/ios/Classes/Mappers.swift
@@ -80,6 +80,7 @@ class Mappers {
             "expMonth": card?.expMonth ?? NSNull(),
             "expYear": card?.expYear ?? NSNull(),
             "last4": card?.last4 ?? NSNull(),
+            "stripeID": card?.stripeID ?? NSNull(),
             "funding": mapFromFunding(card?.funding) ?? NSNull(),
             "name": card?.name ?? NSNull(),
             "address": mapFromAddress(address: card?.address)
